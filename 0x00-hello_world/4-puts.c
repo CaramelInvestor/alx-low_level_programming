f3b6e2145628537c687e38fe604cf5d27e32d390
caramelinvestor <caramelinvestor@gmail.com>
2023-04-20 17:50:48 -0700

    file committed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main -Serves as the entrypoint to the program
+ *
+ *Return: Return zero if no error is encountered,
+ *else return a non-zero number
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
