17ec8c2a78cfdacdf909cbac5ec3671039b6f90b
caramelinvestor <caramelinvestor@gmail.com>
2023-04-20 18:02:46 -0700

    file committed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main (void)
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0:
+/**
+ * main -Serves as the entry point to the program
+ *
+ * Return: Return zero if no error is encountered,
+ * else rreturn a non-zero number
+ */
 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
