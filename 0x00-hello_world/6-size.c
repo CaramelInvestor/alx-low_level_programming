2250703a5ef6080a539cfa6b24c59a6fa754f55e
caramelinvestor <caramelinvestor@gmail.com>
2023-04-20 18:43:13 -0700

    file committed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Serves as the entry point to the program
+ *
+ * Return: Return zero if no enter is encountered ,
+ * else return a non-zero number
+ */
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
