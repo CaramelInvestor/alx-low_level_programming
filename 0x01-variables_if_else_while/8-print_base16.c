bc1abe37b8522f84f21b17fb356878248706bb8b
caramelinvestor <caramelinvestor@gmail.com>
2023-04-21 20:36:08 -0700

    file committed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Serves as the entry point to the program
+ *
+ * Return: Return zero if no error is encountered,
+ * else return a non zero number
+ */
+int main(void)
+{
+	int n;
+	int l;
+
+	for (n = 0; n <= ; n++)
+		putchar(n);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
