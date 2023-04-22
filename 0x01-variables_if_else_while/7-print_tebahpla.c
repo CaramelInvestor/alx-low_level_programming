caa8da153b0671e9210f0b82e7b8c81885c1a0d1
caramelinvestor <caramelinvestor@gmail.com>
2023-04-21 20:11:18 -0700

    file committed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Serves as the entry point to the program
+ *
+ * Return: Return zero if no error is encountered,
+ * else return a non zero number
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
