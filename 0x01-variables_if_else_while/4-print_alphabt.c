1a37d6b36598865a6bbe4a16bfeb2a1322840251
caramelinvestor <caramelinvestor@gmail.com>
2023-04-21 18:19:33 -0700

    file committed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Serves as the entry point to the program
+ *
+ * Return: Return zero if no error is encountered,
+ * else return a non zero number
+ */
+int main(void)
+{
+	char(ch);
+
+
+		for (ch = 'a'; ch < 'e'; ch != 'e'; ch >= 'f'; ch != 'q'; ch >= 'r')
+			putchar(ch);
+		putchar('\n');
+	return (0);
+}
