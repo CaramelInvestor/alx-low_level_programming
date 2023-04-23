b9baa21cab02830f504dcb1401c5ef01456cc362
caramelinvestor <caramelinvestor@gmail.com>
2023-04-23 14:31:49 -0700

    file committed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,17 +18,16 @@ int main(void)
 	{
 		for (m = 49; m <= 57; m++)
 		{
-			putchar(n);
-			putchar(m);
+			if (m > n)
 			{
+				putchar(n);
+				putchar(m);
 				if (n != 56 || m != 57)
-			
 				{
-				
 					putchar(',');
 					putchar(' ');
 				}
-			}
+			}	
 		}
 	}
 	putchar('\n');
