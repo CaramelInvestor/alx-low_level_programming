5843e692026507eba3df08c18444b11a466fe2ae
caramelinvestor <caramelinvestor@gmail.com>
2023-04-25 11:01:53 -0700

    file committed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Serves as an entry point to the program
+ *
+ * Return: Return zero if no error is encountered,
+ * else return a non-zero number
+ */
+
+int main(void)
+{
+	{
+		write("_putchar\n");
+	}
+	return(0)
+}
