fc21177fc2aff967dc3e5de2a5e4ba09311c42bd
caramelinvestor <caramelinvestor@gmail.com>
2023-04-27 14:07:28 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the integer to be checked
+ * @b: the integer to be checked
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
