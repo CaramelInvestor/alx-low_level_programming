2a1af840c3e2bc3bb33b2cfa2c54bdc739eb7686
CaramelInvestor <nnandibekee@gmail.com>
2023-05-02 23:41:30 +0000

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev - a function that prints a string, in reverse, followed by a new line.
+ *@s: srting declared
+ *Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int r;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (r = longi; r > 0; r--)
+	{
+		_putchar(*s);
+		s--;
+	}
+		_putchar('\n');
+}
