eae8be900a6e76c065c8fa968348bbbe88daef01
CaramelInvestor <nnandibekee@gmail.com>
2023-05-13 01:24:16 +0000

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
- *main -  a program that prints its name, followed by a new line
- *@argc: argument count
- *@argv: argument vector
- *Return: if no error return 0
+ * main - print the name of the executable.
+ * @argc: argument count or size of arguments.
+ * @argv: argument vector
+ *
+ * Return: always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
 
