98a7b533f71a19552e71898850d9b4ff19172e9f
CaramelInvestor <nnandibekee@gmail.com>
2023-07-20 12:39:43 +0000

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * dlistint_len - a function that returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - a function that returns the number of
+ * elements in a linked dlistint_t list.
  *@h: pointer to the head node of the list
  * Return: number of element
  */
