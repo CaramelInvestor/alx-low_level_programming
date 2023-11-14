9eb5af9d86b60f6518b0a594785d054b274ef984
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-14 10:33:34 +0100

    search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,15 +11,15 @@
 int linear_search(int *array, size_t size, int value)
 {
     if (array == NULL)
-        return(-1);
+        return (-1);
 
     for (size_t i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
         if (array[i] == value)
-            return(i);
+            return (i);
     }
 
-    return(-1); // Value not found
+    return (-1); // Value not found
 }
